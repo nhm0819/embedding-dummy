c49591b84b1c5071795e0991bc1947d29f539f37
Hongmin <nhm9001@gmail.com>
2025-02-02 14:07:48 +0900

    update docker compose

--- grpc_server.py
+++ grpc_server.py
@@ -5,11 +5,26 @@ from concurrent import futures
 
 import grpc
 import numpy as np
+from grpc.aio import ServerInterceptor
 
 from grpc_app import embedding_pb2, embedding_pb2_grpc
 from grpc_app.enums import BigEndian
 
-logger = logging.getLogger(__name__)
+# 로깅 설정
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+
+class AsyncLoggingInterceptor(ServerInterceptor):
+    async def intercept_service(self, continuation, handler_call_details):
+        logging.info(f"[gRPC Request] Method: {handler_call_details.method}")
+
+        # gRPC 메서드 실행
+        response = await continuation(handler_call_details)
+
+        logging.info(f"[gRPC Response] Method: {handler_call_details.method}")
+        return response
 
 
 class EmbeddingService(embedding_pb2_grpc.EmbeddingServiceServicer):
@@ -36,13 +51,13 @@ class EmbeddingService(embedding_pb2_grpc.EmbeddingServiceServicer):
 
 
 async def async_serve():
-    server = grpc.aio.server()
+    server = grpc.aio.server(interceptors=[AsyncLoggingInterceptor()])
     embedding_pb2_grpc.add_EmbeddingServiceServicer_to_server(
         EmbeddingService(), server
     )
     server.add_insecure_port(f"[::]:{os.getenv('grpc_port', '8004')}")
     await server.start()
-    logger.info(
+    logging.info(
         f"gRPC async server is running on port {os.getenv('grpc_port', '8004')}"
     )
     await server.wait_for_termination()
@@ -59,7 +74,7 @@ def run():
     )
     server.add_insecure_port(f"localhost:{os.getenv('grpc_port', '8004')}")
     server.start()
-    logger.info(f"gRPC server is running on port {os.getenv('grpc_port', '8004')}")
+    logging.info(f"gRPC server is running on port {os.getenv('grpc_port', '8004')}")
     server.wait_for_termination()
 
 
